859f8aa008ed891ba51ca60eb66103aff0a7ddbe
piotreks-uw-edu <>
2023-12-31 10:29:43 +0100

    Fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'allauth.account.middleware.AccountMiddleware',  # new
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = "mysite.urls"
